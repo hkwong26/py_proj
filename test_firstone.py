70703be61dca718ea73324fced9f9221514931b6
Henry <hkwong26@gmail.com>
2021-07-30 15:55:32 -0600

    adding new files

---
+++ test_firstone.py
@@ -0,0 +1,7 @@
+def func(x):
+    return x+1
+
+def test_answer():
+    assert func(4) == 5
+    assert func(4) == 50
+    assert func(1.23) == 2.23
---
+++ test_tmpdir.py
@@ -0,0 +1,3 @@
+def test_tmpdir(tmpdir):
+    print(tmpdir)
+    assert 0
