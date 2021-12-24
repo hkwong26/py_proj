3032756a3965aefe7871e1a4a8ebb03cdbe2202c
toshiba <hkwong26@gmail.com>
2021-12-24 11:51:00 -0700

    Small changes

--- play_house.py
+++ play_house.py
@@ -16,7 +16,9 @@ array_list = list()
 array_list.append("first")
 array_list.append("second")
 array_list.append("third")
+
 array_list.insert(1, "first-second")
+
 array_list.index("third")
 array_list.extend([1, 2, 3])
 array_list.pop()
@@ -25,12 +27,3 @@ cnt = array_list.count(" ")
 array_list.extend(["extent", 3, 3.42, "first"])
 
 set_stuf = set()
-
-# *Dictionary
-hash_dict = dict()
-hash_dict = {"super": 3}
-hash_dict2 = {"third": 4, "super": 454}
-hash_dict["power"] = 34
-hash_dict.update({"first": 3, "second": 45})
-hash_dict.setdefault("super", 0)
-hash_dict.setdefault("batman", 0)
